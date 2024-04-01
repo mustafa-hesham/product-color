628f9bc3e2db90c623ad44b5f8b0f220fc6904a8
Mustafa Hesham <mustafa.hesham@scandiweb.com>
2024-04-01 07:44:19 +0200

    Created convert image to base64 function

--- app/code/AI/ProductColorDetection/Model/DetectColor.php
+++ app/code/AI/ProductColorDetection/Model/DetectColor.php
@@ -13,6 +13,8 @@ use AI\ProductColorDetection\Api\DetectColorInterface;
 use Magento\Framework\Encryption\Helper\Security;
 use AI\ProductColorDetection\Block\Adminhtml\Product\Steps\DetectButton as BlockDetectColor;
 use Magento\Framework\Exception\AuthenticationException;
+use Magento\Framework\Exception\LocalizedException;
+use Exception;
 
 class DetectColor implements DetectColorInterface
 {
@@ -43,8 +45,8 @@ class DetectColor implements DetectColorInterface
             throw new AuthenticationException(__('The detect color key is not valid.'));
         }
 
-        $sessionDetectKey = $this->blockDetectColor->getDetectColorKey();
-        $formKey = $data['detect_color_key'];
+        $imagePath = $data['image'];
+        $base64 = $this->convertImageToBase64($imagePath);
 
         $resultData = [
             'color' => 'Black',
@@ -52,5 +54,24 @@ class DetectColor implements DetectColorInterface
         ];
 
         return json_encode($resultData);
+    }
+
+
+    /**
+     * Converts image to base64 encoding.
+     *
+     * @param string $imagePath
+     * @return string
+     * @throws LocalizedException
+     */
+    public function convertImageToBase64(string $imagePath): string
+    {
+        try {
+            $type = pathinfo($imagePath, PATHINFO_EXTENSION);
+            $data = file_get_contents($imagePath);
+            return 'data:image/' . $type . ';base64,' . base64_encode($data);
+        } catch (Exception $e) {
+            throw new LocalizedException(__('Please provide a valid image path. Error: ' . $e->getMessage()));
+        }
     }
 }
