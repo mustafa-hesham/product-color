58a620b77fbf05fb0b363c9c4e80cf798d5a123b
Mustafa Hesham <mustafa.hesham@scandiweb.com>
2024-04-24 04:44:05 +0200

    Added top color to the suggestions when detecting colors

--- app/code/TheArchitectAI/ProductColorDetection/view/adminhtml/web/js/detectButton.js
+++ app/code/TheArchitectAI/ProductColorDetection/view/adminhtml/web/js/detectButton.js
@@ -27,8 +27,39 @@ define([
             this.bindCustomEvent();
         },
 
+        componentToHex: function(c) {
+            var hex = c.toString(16);
+            return hex.length == 1 ? "0" + hex : hex;
+        },
+
         getRGBColorText: function (color) {
             return "rgb("+ color[0] +"," + color[1] + "," + color[2] + ")"
+        },
+
+        rgbToHex: function(r, g, b) {
+            return "#" + this.componentToHex(r) + this.componentToHex(g) + this.componentToHex(b);
+        },
+
+        addTopColor: function(colorValue, colorName) {
+            var html = '';
+            const hexValue = this.rgbToHex(colorValue[0], colorValue[1], colorValue[2]);
+            const colorNameLabel = $.mage.__('Color Name');
+            const color = $.mage.__('Color');
+            const colorNameComment = $.mage.__('Suggested color name.');
+
+            html += '<div class="admin__field DetectButton-ColorWrapper">';
+            html += `<label for="DetectButton-ColorValue" class="admin__field-label"><span>${color}</span></label>`;
+            html += `<input id="DetectButton-ColorValue" style="background-color: ${this.getRGBColorText(colorValue)};" class="admin__control-text DetectButton-ColorValue" type="text" readonly />`;
+            html += `<label for="DetectButton-ColorName" class="admin__field-label"><span>${colorNameLabel}</span></label>`;
+            html += '<div class="DetectButton-InputWrapper">';
+            html += `<input id="DetectButton-ColorName" class="admin__control-text DetectButton-ColorName" type="text" value="${colorName}" />`;
+            html += `<div class="admin__field-note">${colorNameComment}</div>`;
+            html += '</div>';
+            html += '<label for="DetectButton-ColorHex" class="admin__field-label"><span>Hex</span></label>';
+            html += `<input id="DetectButton-ColorHex" class="admin__control-text DetectButton-ColorHex" type="text" value="${hexValue}" readonly />`;
+            html += '</div>';
+
+            return html;
         },
 
         sendPostRequest: async function(image) {
@@ -82,9 +113,15 @@ define([
                     top_colors
                 } = await self.sendPostRequest(self.images[0]);
 
-                $(this).siblings(".DetectButton-ColorWrapper").find('#DetectButton-ColorValue').css({ "background-color": self.getRGBColorText(object_dominant_color_rgb) });
-                $(this).siblings(".DetectButton-ColorWrapper").find('#DetectButton-ColorName').val(approximate_color_name).css('textTransform', 'capitalize');
-                $(this).siblings(".DetectButton-ColorWrapper").find('#DetectButton-ColorHex').val(object_dominant_color_hex);
+                $(this).siblings(".DetectButton-Colors").find('#DetectButton-ColorValue').css({ "background-color": self.getRGBColorText(object_dominant_color_rgb) });
+                $(this).siblings(".DetectButton-Colors").find('#DetectButton-ColorName').val(approximate_color_name);
+                $(this).siblings(".DetectButton-Colors").find('#DetectButton-ColorHex').val(object_dominant_color_hex);
+                $(this).siblings(".DetectButton-Colors").find('.DetectButton-TopColors').html('');
+
+                top_colors.forEach((color) => {
+                    $(this).siblings(".DetectButton-Colors").find('.DetectButton-TopColors').append(self.addTopColor(color[1], color[2]))
+                });
+
                 console.log(object_dominant_color_rgb, approximate_color_name, top_colors);
             });
         },
