b1a59fe9277ffef4a7f7f657eabf473d8104d463
Mustafa Hesham <mustafa.hesham@scandiweb.com>
2024-05-11 15:34:03 +0300

    Rename TheArchitectAI to TheArchitect

---
+++ app/code/TheArchitect/ProductColorDetection/registration.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * TheArchitect_ProductColorDetection
+ *
+ * @category TheArchitect
+ * @package TheArchitect_ProductColorDetection
+ */
+
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'TheArchitect_ProductColorDetection',
+    __DIR__
+);
